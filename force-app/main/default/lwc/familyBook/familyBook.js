917de2a8026e35cae871a0609852152caabe8bbd
nova-miniPC <singhamar01@gmail.com>
2025-06-01 00:28:45 +0900

    familyBook Initial version

--- force-app/main/default/lwc/familyBook/familyBook.js
+++ force-app/main/default/lwc/familyBook/familyBook.js
@@ -1,11 +1,27 @@
 import { LightningElement } from 'lwc';
 import { PersonModuleMixin } from 'c/vi_ApexModule';
 export default class FamilyBook extends PersonModuleMixin(LightningElement) {
+    //crate an array to hold OrderLevel data from 1 to 6
+    orderlevels = [1, 2, 3, 4, 5, 6];
     //create a variable to hold the persons
     persons = [];
+    groupedPersons = [];
     async connectedCallback() {
         console.log('FamilyBook connectedCallback called');
         this.persons = await this.getAllPersons();
-        console.log('All Persons:', JSON.stringify(this.persons));
+        this.groupedPersons = this.groupPersonsByOrderLevel(this.persons);
+        //console.log('All Persons:', JSON.stringify(this.persons));
+        console.log('Grouped Persons:', JSON.stringify(this.groupedPersons));
+    }
+    groupPersonsByOrderLevel(persons) {
+        const grouped = {};
+        persons.forEach(person => {
+            const orderLevel = person.Order_Level__c;
+            if (!grouped[orderLevel]) {
+                grouped[orderLevel] = { orderLevel, persons: [] };
+            }
+            grouped[orderLevel].persons.push(person);
+        });
+        return Object.values(grouped);
     }
 }
