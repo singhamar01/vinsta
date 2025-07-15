a363fab5034184a610b5116af5b3e160ba931f03
nova-miniPC <singhamar01@gmail.com>
2025-07-15 21:04:51 +0900

    final working version

--- force-app/main/default/lwc/customCheckbox/customCheckbox.js
+++ force-app/main/default/lwc/customCheckbox/customCheckbox.js
@@ -1,10 +1,18 @@
 import { LightningElement } from 'lwc';
 
 export default class CustomCheckbox extends LightningElement {
+
+    handleCheckboxChange(event) {
+        const isChecked = event.target.checked;
+        const checkbox = this.template.querySelector('lightning-input');
+        // Dynamically update CSS custom property based on checkbox state
+        checkbox.style.setProperty('--checkbox-background', isChecked ? '#28a745' : '#f0f099');
+    }
+    /* 
     handleCheckboxChange(event) {
         const isChecked = event.target.checked;
         const container = this.template.querySelector('.checkbox-container');
         // Dynamically update CSS custom property based on checkbox state
         container.style.setProperty('--checkbox-background', isChecked ? '#28a745' : '#e410a4ff');
-    }
+    }*/
 }
