edb9f800d821c64624f69931e601aac3a3367fcc
nova-miniPC <singhamar01@gmail.com>
2025-07-15 20:48:54 +0900

    partially working

--- force-app/main/default/lwc/customCheckbox/customCheckbox.js
+++ force-app/main/default/lwc/customCheckbox/customCheckbox.js
@@ -1,34 +1,10 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement } from 'lwc';
 
 export default class CustomCheckbox extends LightningElement {
-  @api componentType;
-  @api recordId;
-  @api objectApiName;
-  @api isDisabled = false;
-  @track isChecked = true;
-
-  get wrapperClass() {
-    let baseClass = 'slds-m-around_small';
-    if (this.isDisabled) {
-      return this.isChecked
-        ? `${baseClass} custom-checkbox-grey`
-        : `${baseClass} custom-checkbox-black`;
+    handleCheckboxChange(event) {
+        const isChecked = event.target.checked;
+        const container = this.template.querySelector('.checkbox-container');
+        // Dynamically update CSS custom property based on checkbox state
+        container.style.setProperty('--checkbox-background', isChecked ? '#28a745' : '#f0f099');
     }
-    return this.isChecked
-      ? `${baseClass} custom-checkbox-green`
-      : baseClass;
-  }
-  get wrapperStyle() {
-    return this.isChecked && !this.isDisabled ? 'position: relative; padding: 0.5rem;' : 'padding: 0.5rem;';
-  }
-
-  handleCheckboxChange(event) {
-    if (!this.isDisabled) {
-      this.isChecked = event.target.checked;
-    }
-  }
-
-  renderedCallback() {
-    console.log('Wrapper Class:', this.wrapperClass, 'isChecked:', this.isChecked, 'isDisabled:', this.isDisabled);
-  }
 }
